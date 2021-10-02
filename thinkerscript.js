e01c21559c44ff9a13ac219f60351c56daca4330
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-01 20:17:49 -0400

    Set up fetch request and test

--- thinkerscript.js
+++ thinkerscript.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const tags = 'love';
+
+fetch(`https://api.quotable.io/random?tags=${tags}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(`${data.content} —${data.author}`)
+  })
+  .catch((error) => {
+    console.log(`ERROR: ${error}`);
+  })
