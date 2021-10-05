e6bac328fbf0c3c0bd3ccb15e3849768eddec64a
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-05 18:15:02 -0400

    added new word function

--- thinkerscript.js
+++ thinkerscript.js
@@ -1,20 +1,20 @@
 
 
-/*======================Fetch API Call======================*/
-//=============== onClick Function & Append ==================
+/*======================Fetch API Calls======================*/
+//=============== onClick Functions & Appends =================
+//=============== Random Quote API =========================
 const quoteSpace = document.querySelector('#quotearea');
 const buttonTest = document.querySelector('#genbtn')
+const wordButton = document.querySelector('#wordbtn');
 
 buttonTest.addEventListener('click', () => {
-  console.log('clicked');
 
   fetch(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
     .then((result) => {
       return result.json();
     })
     .then((data) => {
-      console.log(data);
-
+    
       const quoteContent = data.data[0].quoteText;
       const quoteAuthor = data.data[0].quoteAuthor;
   
@@ -26,8 +26,6 @@ buttonTest.addEventListener('click', () => {
       displayContent.classList.add('display-content');
       displayAuthor.classList.add('display-author');
 
-
-  
       displayContent.append(quoteContent);
       displayAuthor.append(quoteAuthor);
       displayQuote.append(displayContent, displayAuthor);
@@ -39,6 +37,41 @@ buttonTest.addEventListener('click', () => {
     })
 })
 
+// ========== Word API Fetch and onClick Functions ==============
+
+wordButton.addEventListener('click', () => {
+  console.log('clicked');
+
+
+fetch(`https://random-words-api.vercel.app/word`)
+  .then((result) => {
+    return result.json();
+  }).then((data) => {
+    console.log(data);
+
+    const wordName = data[0].word;
+    const wordPronounce = data[0].pronunciation;
+    const wordDefinition = data[0].definition;
+
+    const displayWordDiv = document.createElement('div');
+    const displayWord = document.createElement('p');
+    const displayPronounce = document.createElement('p');
+    const displayDefinition = document.createElement('p');
+
+    displayWordDiv.classList.add('word-div');
+    displayWord.classList.add('word-name');
+    displayPronounce.classList.add('word-pron');
+    displayDefinition.classList.add('word-def');
+
+    displayWord.append(`New Word: ${wordName}`);
+    displayPronounce.append(`How to Pronounce: ${wordPronounce}`);
+    displayDefinition.append(`What it means: ${wordDefinition}`);
+
+    displayWordDiv.append(displayWord, displayPronounce, displayDefinition);
+
+    quoteSpace.append(displayWordDiv);
+})
+})
 // ======================= Meditate Feature ==================== //
 
 const meditateButton = document.querySelector('#meditatebtn');
